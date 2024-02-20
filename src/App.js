9d3d9e1a8b985ac28a93c014056895ad7418c5b2
sezin akbulut <sezin.akbulut@gmail.com>
2024-02-20 10:34:24 +0100

    add useEffect to app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 import Header from "./Header";
 import Form from "./Form";
 import Todo from "./Todo";
 
+const LSKEY = "MyTodoApp";
+
 function App() {
-  const [todos, setTodos] = useState([]);
+  const initialTodos = [];
+  const [todos, setTodos] = useState(initialTodos);
 
   const addTodo = (newTodo) => {
-    setTodos([...todos, newTodo]);
+    setTodos([...todos, { id: uuidv4(), todo: newTodo, completed: false }]);
   };
+
+  useEffect(() => {
+    window.localStorage.setItem(LSKEY + ".todos", JSON.stringify(todos));
+  });
 
   return (
     <div>
