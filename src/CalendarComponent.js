8746605e7040d22b538c87bbb343f4bd8ee307a5
sezin akbulut <sezin.akbulut@gmail.com>
2024-02-22 10:47:52 +0100

    update calendar default month

--- src/CalendarComponent.js
+++ src/CalendarComponent.js
@@ -6,6 +6,7 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 const localizer = momentLocalizer(moment);
 
 const CalendarComponent = ({ todos }) => {
+  <Calendar localizer={localizer} style={{ height: 500 }} />;
   const events = todos.map((todo) => ({
     title: todo.text,
     start: new Date(todo.fromDate),
@@ -17,7 +18,18 @@ const CalendarComponent = ({ todos }) => {
   return (
     <div>
       <h1>Calendar</h1>
-      <Calendar localizer={localizer} events={events} />
+
+      <Calendar
+        localizer={localizer}
+        defaultView="month"
+        events={events}
+        style={{
+          height: "70vh",
+          backgroundColor: "#5F8670",
+          width: "100%",
+          whiteSpace: "nowrap",
+        }}
+      />
     </div>
   );
 };
